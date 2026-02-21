d1d77397caf235c238a4066b26c66342b5f92cc1
Zoey Zhang <zoeyzhang1257@gmail.com>
2026-02-21 21:47:22 +0000

    The google sign-in is still failing. It seems that you did not complete

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 'use client';
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDummyKey",
-  authDomain: "ualright-studio.firebaseapp.com",
-  projectId: "ualright-studio",
-  storageBucket: "ualright-studio.firebasestorage.app",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyDd_qikkxd5_ZP_7EmQrs0kViCbs5W4YH4",
+  authDomain: "studio-5279848450-a5e2d.firebaseapp.com",
+  projectId: "studio-5279848450-a5e2d",
+  storageBucket: "studio-5279848450-a5e2d.firebasestorage.app",
+  messagingSenderId: "907797777478",
+  appId: "1:907797777478:web:816beed104b087b2948e47"
 };
